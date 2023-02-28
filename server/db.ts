9ebdc726ccd409a4f61708000c4e76d6a6e41e6e
Eugenio <eugeniolgimenez@gmail.com>
2023-02-28 16:19:55 -0300

    serviceAccount

--- server/db.ts
+++ server/db.ts
@@ -4,22 +4,22 @@ console.log("process.env.DATABASE_URL: ", process.env.DATABASE_URL);
 import * as admin from "firebase-admin";
 import * as firebaseKey from "./key.json";
 
-// const serviceAccount = {
-//   type: "service_account",
-//   project_id: process.env.FIREBASE_PROJECT_ID,
-//   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-//   private_key: process.env.FIREBASE_PRIVATE_KEY,
-//   client_email: process.env.FIREBASE_CLIENT_MAIL,
-//   client_id: process.env.FIREBASE_CLIENT_ID,
-//   auth_uri: process.env.FIREBASE_AUTH_URI,
-//   token_uri: process.env.FIREBASE_TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT,
-//   client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
-// };
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_MAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_CERT,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT,
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseKey as any),
+    credential: admin.credential.cert((firebaseKey as any) || serviceAccount),
     databaseURL: process.env.DATABASE_URL,
   });
 }
